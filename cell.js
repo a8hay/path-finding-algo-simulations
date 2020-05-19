e1faecea7dfe8925bfe0ba382ad4372d533416e5
Abhay kumar singh <46368902+a8hay@users.noreply.github.com>
2020-05-19 22:01:06 +0530

    Update cell.js

--- cell.js
+++ cell.js
@@ -34,4 +34,18 @@ class Cell {
     fill(this.clr[0], this.clr[1], this.clr[2], this.clr[3]);
     rect(this.x, this.y, cellSize, cellSize);
   }
+
+  // copy without neighbours
+  shallowCopy() {
+    let copy = new Cell(this.x, this.y);
+
+    copy.isStart = this.isStart;
+    copy.isEnd = this.isEnd;
+    copy.isWall = this.isWall;
+    copy.isDiscovered = this.isDiscovered;
+    copy.isPath = this.isPath;
+    copy.clr = [].concat(this.clr);
+
+    return copy;
+  }
 }
